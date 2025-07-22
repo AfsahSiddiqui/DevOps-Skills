a63bb9b5d8804abcd1f7f47fe9a422555f2264c7
AfsahSiddiqui <109294675+AfsahSiddiqui@users.noreply.github.com>
2025-07-22 00:50:35 -0400

    Update backup.sh

--- Backing-up-with-Docker/backup.sh
+++ Backing-up-with-Docker/backup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Set target to backup: either "host_dir" or a running container
-target=$1  # e.g., ./backup.sh host or ./backup.sh container
+# Set target to backup: either "host" or a "container"
+target=$1 
+# Set source to be backed up: either a path to folder or a container name
+source=$2  
 
-# Directory where backups will be stored on the EC2 instance
+if [[ -z "$target" || -z "$source" ]]; then
+    echo "Usage: $0 [host|container] <source_path_or_container_name>"
+    exit 1
+fi
+
+
+# Directory where backups will be stored 
 backup_root="/backup_data"
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 backup_path="${backup_root}/${timestamp}"
@@ -11,23 +19,21 @@ backup_path="${backup_root}/${timestamp}"
 # Name of Docker backup image
 backup_tool_image="backup_tool:latest"
 
-# Container to back up (if using container option)
-source_container="my_app_container"
-
 mkdir -p "$backup_path"
 
 if [[ "$target" == "host" ]]; then
     # Backup a folder from EC2 (edit path as needed)
-    echo "Backing up /home/ubuntu/app-data from EC2..."
-    docker run --rm -v /home/ubuntu/app-data:/source -v "$backup_path:/backup" "$backup_tool_image"
+    echo "Backing up host path: $source..."
+    docker run --rm -v "$source:/source" -v "$backup_path:/backup" "$backup_tool_image"
 
 elif [[ "$target" == "container" ]]; then
     # Backup volumes from another container
-    echo "Backing up volumes from container $source_container..."
-    docker run --rm --volumes-from "$source_container" -v "$backup_path:/backup" "$backup_tool_image"
+    echo "Backing up volumes from container: $source..."
+    docker run --rm --volumes-from "$source" -v "$backup_path:/backup" "$backup_tool_image"
 
 else
-    echo "Usage: $0 [host|container]"
+    echo "Invalid target type: $target"
+    echo "Usage: $0 [host|container] <source_path_or_container_name>"
     exit 1
 fi
 
